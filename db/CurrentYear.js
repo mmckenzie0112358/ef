5eeada365876c1234fc849a72591911d30bf5e48
Michael McKenzie <michael@ionthree.com>
2019-12-30 09:22:36 -0500

    added efficiency

---
+++ db/CurrentYear.js
@@ -0,0 +1,10 @@
+[
+  {
+    "Sales YTD": 20.0,
+    "Gallons Delivered": 12,
+    "Number Of Deliveries": 6,
+    "Average Gallons Delivered": 45,
+    "Average Sales Amount": 9.45,
+    "Delivery Efficiency": 14.2
+  }
+];

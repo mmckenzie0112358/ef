caa900633fb5ad4a05783f6824dccd13eac0e54e
Michael McKenzie <michael@ionthree.com>
2020-01-03 15:56:05 -0500

    added TypesAndCodes

---
+++ db/TankInfo/TypesAndCodes.js
@@ -0,0 +1,35 @@
+[
+  {
+    CustomerKey: "Commercial",
+    DeliveryType: "Commercial",
+    RegulatoryCode: "Commercial",
+    PublicAssembly: "true",
+    Underground: "false",
+    Cathodic: "02/21/19",
+    PassOrFail: "Pass",
+    SalesYTD: 734.17,
+    GallonsUsedYTD: 8179.93
+  },
+  {
+    CustomerKey: "Residential",
+    DeliveryType: "Commercial",
+    RegulatoryCode: "Residential",
+    PublicAssembly: "false",
+    Underground: "true",
+    Cathodic: "02/21/19",
+    PassOrFail: "Pass",
+    SalesYTD: 230.19,
+    GallonsUsedYTD: 2300.01
+  },
+  {
+    CustomerKey: "Commercial",
+    DeliveryType: "Commercial",
+    RegulatoryCode: "Commercial",
+    PublicAssembly: "true",
+    Underground: "false",
+    Cathodic: "02/21/19",
+    PassOrFail: "Pass",
+    SalesYTD: 675.14,
+    GallonsUsedYTD: 3187.53
+  }
+];

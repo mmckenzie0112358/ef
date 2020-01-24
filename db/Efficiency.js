61bb1cc44b53b9ca0a589856f65eb3f19c6b5f0f
Michael McKenzie <michael@ionthree.com>
2020-01-24 12:51:25 -0500

    Added Efficiency data that combined current, previous and ,prior

---
+++ db/Efficiency.js
@@ -0,0 +1,23 @@
+[
+  {
+    index: 0,
+    CurrentSalesYTD: "20.00",
+    CurrentGallonsDelivered: 12,
+    CurrentNumberOfDeliveries: 6,
+    CurrentAverageGallonsDelivered: 45,
+    CurrentAverageSalesAmount: "9.45",
+    CurrentDeliveryEfficiency: "14.2",
+    LastSalesYTD: "25.00",
+    LastGallonsDelivered: 16,
+    LastNumberOfDeliveries: 2,
+    LastAverageGallonsDelivered: 40,
+    LastAverageSalesAmount: "67.55",
+    LastDeliveryEfficiency: "17.2",
+    PriorSalesYTD: "500.00",
+    PriorGallonsDelivered: 20,
+    PriorNumberOfDeliveries: 10,
+    PriorAverageGallonsDelivered: 50,
+    PriorAverageSalesAmount: "40.23",
+    PriorDeliveryEfficiency: "16.4"
+  }
+];
